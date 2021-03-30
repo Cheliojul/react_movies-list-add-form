39435aa1b7d4e404ad02870ee541bf2538baa510
Nikita Puhovskii <cheliojul@gmail.com>
2021-03-30 14:10:46 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2088c32903613710f8f9678a01b40cd4.js"
+  "./precache-manifest.29b055dbb35f0c47aab50be5a0f628e8.js"
 );
 
 self.addEventListener('message', (event) => {
